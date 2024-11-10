a25ca52a9f513ce0134fc4fa6a36f2044b7c69e8
Rebekah Midkiff <rmidkiff@microsoft.com>
2024-11-10 09:14:02 -0800

    Initial commit

---
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -0,0 +1,25 @@
+﻿#pragma warning disable SKEXP0050
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddAzureOpenAIChatCompletion(
+    "gpt-35-turbo-16k-rebmid",
+    "https://openai-rebmid.openai.azure.com/",
+    "FeGq8JEJdloPwWbK23vKsWXrdzkatUfDpeQuypEo6iBoHeCJadRdJQQJ99AKACYeBjFXJ3w3AAABACOGNfvg");
+
+builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+var kernel = builder.Build();
+
+string input = @"Please create a list of vegan breakfast recipes and gather ideas for spicy vegan dishes. 
+Also, check if there are vegan-friendly ingredients available in local stores.";
+
+var result = await kernel.InvokeAsync(
+    "ConversationSummaryPlugin", 
+    "GetConversationActionItems", 
+    new() {{ "input", input }});
+
+Console.WriteLine(result);
---
+++ M04-combine-prompts-and-functions/M04-Project/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourApiKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourApiKey,
+    "gpt-35-turbo-16k");
+var kernel = builder.Build();
+
+kernel.ImportPluginFromType<MusicLibraryPlugin>();
+
+var result = await kernel.InvokeAsync(
+    "MusicLibraryPlugin", 
+    "AddToRecentlyPlayed", 
+    new() {
+        ["artist"] = "Tiara", 
+        ["song"] = "Danse", 
+        ["genre"] = "French pop, electropop, pop"
+    }
+);
+
+Console.WriteLine(result);
