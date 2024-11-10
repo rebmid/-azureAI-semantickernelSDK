89f068efab0eea43f1fd36693e6bc532b55fcc6c
rebmid <rebmid@gmail.com>
2024-11-10 11:46:52 -0800

    Automatically invoked functions

--- M05-use-intelligent-planners/M05-Project/Program.cs
+++ M05-use-intelligent-planners/M05-Project/Program.cs
@@ -1,15 +1,28 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
-string yourDeploymentName = "";
-string yourEndpoint = "";
-string yourApiKey = "";
+
 
 var builder = Kernel.CreateBuilder();
 builder.AddAzureOpenAIChatCompletion(
-    yourDeploymentName,
-    yourEndpoint,
-    yourApiKey,
-    "gpt-35-turbo-16k");
-var kernel = builder.Build();
+    "openAImodeldeploymentname",
+    "openAIendpoint",
+    "APIkey");
 
+    var kernel = builder.Build();
+    kernel.ImportPluginFromType<MusicLibraryPlugin>();
+    kernel.ImportPluginFromType<MusicConcertPlugin>();
+    kernel.ImportPluginFromPromptDirectory("Prompts");
+
+    OpenAIPromptExecutionSettings settings = new()
+    {
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+    };
+
+    string prompt = @"I live in Portland OR USA. Based on my recently 
+        played songs and a list of upcoming concerts, which concert 
+        do you recommend?";
+
+var result = await kernel.InvokePromptAsync(prompt, new(settings));
+
+Console.WriteLine(result);
